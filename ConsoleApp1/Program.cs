ca2fda65096d4550fb0d6570812beada948a2668
Seung Soo Kim <iarihs@naver.com>
2025-02-04 09:17:40 +0900

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿void PrintLine()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        Console.Write("=");
+    }
+    Console.WriteLine();
+}
+
+void PrintLine2(int count)
+{
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write("=");
+    }
+    Console.WriteLine();
+}
+
+int Add(int a, int b)
+{
+    return a + b;
+}
+
+PrintLine();
+PrintLine2(5);
+
+int result = Add(10, 20);
+Console.WriteLine(result);
